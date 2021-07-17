98dfc2a663306a0cb1ac34147e86607d7a5d1a13
DanBasilaev <mr.basilaev@gmail.com>
2021-07-17 16:49:53 +0300

    Post and get

--- xsolla/main.go
+++ xsolla/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
 )
+
+type item struct {
+	id int
+	sku string
+	name string
+	category string
+	price int
+}
 
 func main() {
 	r := gin.Default()
@@ -17,16 +24,43 @@ func main() {
 	defer db.Close()
 
 
-	insert, err := db.Query("INSERT INTO items (sku, name, category, price) VALUES ('12jfdd2','PS5','console','49990')")
+	/*insert, err := db.Query("INSERT INTO items (sku, name, category, price) VALUES ('12jfdd2','PS5','console','49990')")
 	if err != nil {
 		panic(err)
 	}
 	defer insert.Close()
-	fmt.Println("Состыковочка")
+	fmt.Println("Состыковочка")*/
 
-	
+
 	//Создание товара
-//	r.POST("/", func(c *gin.Context) {})
+	r.POST("/addItem", func(c *gin.Context) {
+		sku := c.Query("sku")
+		name := c.Query("name")
+		category := c.Query("category")
+		price := c.Query("price")
+
+		insert, err := db.Query("INSERT INTO items (sku, name, category, price) VALUES (?, ?, ?, ?)", sku, name, category, price)
+		if err != nil {
+			panic(err)
+		}
+
+		defer insert.Close()
+
+		get_id, err := db.Query("SELECT id FROM items where sku=?", sku)
+		if err != nil {
+			panic(err)
+		}
+
+		get_id.Next()
+		var id int
+		err = get_id.Scan(&id)
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, gin.H{"id":id})
+
+		})
 
 	//Редактирование товара
 	//r.PUT("/", func(c *gin.Context) {})
@@ -35,7 +69,23 @@ func main() {
 	//r.DELETE("/", func(c *gin.Context) {})
 
 	//Получение информации о товаре по его идентификатору или SKU
-	//r.GET("/", func(c *gin.Context) {})
+	r.GET("/getItem", func(c *gin.Context) {
+		id := c.Query("id")
+		get_item, err := db.Query("SELECT * FROM items where id=?", id)
+		if err != nil {
+			panic(err)
+		}
+
+		get_item.Next()
+		var itm item
+		err = get_item.Scan(&itm.id, &itm.sku, &itm.name, &itm.category, &itm.price)
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, gin.H{"id":itm.id, "sku":itm.sku, "name":itm.name, "category":itm.category, "price":itm.price})
+
+	})
 
 	//Получение каталога товаров
 	/*r.GET("/", func(c *gin.Context) {
